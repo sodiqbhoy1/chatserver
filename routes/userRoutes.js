58e99055cdd43e21b43d2233a830cdf42af1267e
sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-08-21 16:48:30 +0100

    fix: update user registration route path for consistency

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/register", userController.userRegister);
+router.post("/user/register", userController.userRegister);
 router.get("/users", userController.fetchAllUsers);
 router.delete("/users/:id", userController.deleteUser);
 
